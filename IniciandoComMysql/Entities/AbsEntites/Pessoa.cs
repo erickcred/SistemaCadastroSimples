023a3bc315d9c92427d28e54375dc36f5d02f76b
Erick Rick <erickcredjes@gmail.com>
2022-05-11 20:41:00 -0300

    Criando as entidades

---
+++ IniciandoComMysql/Entities/AbsEntites/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IniciandoComMysql.Entities.AbsEntites
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+
+        public Pessoa() { }
+        public Pessoa(int id, string nome, string email, string telefone, string endereco)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Telefone = telefone;
+            Endereco = endereco;
+        }
+
+    }
+}
---
+++ IniciandoComMysql/Entities/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using IniciandoComMysql.Entities.AbsEntites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IniciandoComMysql.Entities
+{
+    public class Cliente : Pessoa
+    {
+        public Cliente() { }
+        public Cliente(int id, string nome, string email, string telefone, string endereco) : base(id, nome, email, telefone, endereco) { }
+    }
+}
