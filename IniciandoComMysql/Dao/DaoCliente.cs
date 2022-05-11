bfd4cace33c11ce675a061f2b5e5b37612299253
Erick Rick <erickcredjes@gmail.com>
2022-05-11 20:39:10 -0300

    Configuração basica da camada de Dados

---
+++ IniciandoComMysql/Dao/DaoCliente.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IniciandoComMysql.Entities;
+using MySql.Data.MySqlClient;
+
+namespace IniciandoComMysql.Dao
+{
+    public class DaoCliente
+    {
+        
+        MySqlConnection Conexao;
+
+        public void Salvar(Cliente cliente)
+        {
+            try
+            {
+                Conexao = new MySqlConnection(DaoConexao.Conexao);
+                Conexao.Open();
+                
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = Conexao;
+                comando.Prepare();
+                comando.CommandText = "INSERT INTO contato (nome, email, telefone, endereco) VALUES(@nome, @email, @telefone, @endereco)";
+                
+                comando.Parameters.AddWithValue("@nome", cliente.Nome);
+                comando.Parameters.AddWithValue("@email", cliente.Email);
+                comando.Parameters.AddWithValue("@telefone", cliente.Telefone);
+                comando.Parameters.AddWithValue("@endereco", cliente.Endereco);
+
+                comando.ExecuteNonQuery();
+            } catch (MySqlException erro)
+            {
+                throw new Exception(erro.Message);
+            } finally
+            {
+                Conexao.Close();
+            }
+        }
+
+        public List<Cliente> Pesquisa()
+        {
+            try
+            {
+                Conexao = new MySqlConnection(DaoConexao.Conexao);
+                Conexao.Open();
+
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = Conexao;
+                comando.Prepare();
+                comando.CommandText = "SELECT * FROM contato";
+
+                MySqlDataReader reader = comando.ExecuteReader();
+
+                List<Cliente> clientes = new List<Cliente>();
+
+                while (reader.Read())
+                {
+                    clientes.Add(new Cliente(
+                        reader.GetInt32(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                        reader.GetString(4)
+                        )
+                    );
+                }
+                return clientes;
+            } catch (MySqlException erro)
+            {
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+
+        public List<Cliente> Pesquisa(string text)
+        {
+            try
+            {
+                Conexao = new MySqlConnection(DaoConexao.Conexao);
+                Conexao.Open();
+
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = Conexao;
+                comando.Prepare();
+                comando.CommandText = "SELECT * FROM contato WHERE nome LIKE @nome OR email LIKE @nome";
+
+                comando.Parameters.AddWithValue("@nome", $"%{text}%");
+                MySqlDataReader reader = comando.ExecuteReader();
+
+                List<Cliente> clientes = new List<Cliente>();
+                while (reader.Read())
+                {
+                    clientes.Add(new Cliente(
+                        reader.GetInt32(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                        reader.GetString(4)
+                    ));
+                }
+                return clientes;
+
+            } catch (MySqlException erro)
+            {
+                throw new Exception(erro.Message);
+            } catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+
+        }
+    }
+
+}
---
+++ IniciandoComMysql/Dao/DaoConexao.cs
@@ -0,0 +1,14 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IniciandoComMysql.Dao
+{
+    public static class DaoConexao
+    {
+        public static string Conexao = "datasource=localhost;username=root;password=root;database=db_contato";
+    }
+}
