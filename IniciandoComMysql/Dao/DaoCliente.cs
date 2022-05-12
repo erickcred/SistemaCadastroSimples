431762f096013b082b323c67b625bd5112b51123
Erick Rick <erickcredjes@gmail.com>
2022-05-11 22:19:36 -0300

    Implementando a edição do conteúdo

--- IniciandoComMysql/Dao/DaoCliente.cs
+++ IniciandoComMysql/Dao/DaoCliente.cs
@@ -40,7 +40,45 @@ namespace IniciandoComMysql.Dao
             }
         }
 
-        public List<Cliente> Pesquisa()
+        //public List<Cliente> Pesquisa()
+        //{
+        //    try
+        //    {
+        //        Conexao = new MySqlConnection(DaoConexao.Conexao);
+        //        Conexao.Open();
+
+        //        MySqlCommand comando = new MySqlCommand();
+        //        comando.Connection = Conexao;
+        //        comando.Prepare();
+        //        comando.CommandText = "SELECT * FROM contato";
+
+        //        MySqlDataReader reader = comando.ExecuteReader();
+
+        //        List<Cliente> clientes = new List<Cliente>();
+
+        //        while (reader.Read())
+        //        {
+        //            clientes.Add(new Cliente(
+        //                reader.GetInt32(0),
+        //                reader.GetString(1),
+        //                reader.GetString(2),
+        //                reader.GetString(3),
+        //                reader.GetString(4)
+        //                )
+        //            );
+        //        }
+        //        return clientes;
+        //    } catch (MySqlException erro)
+        //    {
+        //        throw new Exception(erro.Message);
+        //    }
+        //    finally
+        //    {
+        //        Conexao.Close();
+        //    }
+        //}
+
+        public List<Cliente> Pesquisa(string text = "")
         {
             try
             {
@@ -50,47 +88,16 @@ namespace IniciandoComMysql.Dao
                 MySqlCommand comando = new MySqlCommand();
                 comando.Connection = Conexao;
                 comando.Prepare();
-                comando.CommandText = "SELECT * FROM contato";
-
-                MySqlDataReader reader = comando.ExecuteReader();
-
-                List<Cliente> clientes = new List<Cliente>();
-
-                while (reader.Read())
+                
+                if (text != "")
                 {
-                    clientes.Add(new Cliente(
-                        reader.GetInt32(0),
-                        reader.GetString(1),
-                        reader.GetString(2),
-                        reader.GetString(3),
-                        reader.GetString(4)
-                        )
-                    );
+                    comando.CommandText = "SELECT * FROM contato WHERE nome LIKE @nome OR email LIKE @nome";
+                    comando.Parameters.AddWithValue("@nome", $"%{text}%");
                 }
-                return clientes;
-            } catch (MySqlException erro)
-            {
-                throw new Exception(erro.Message);
-            }
-            finally
-            {
-                Conexao.Close();
-            }
-        }
-
-        public List<Cliente> Pesquisa(string text)
-        {
-            try
-            {
-                Conexao = new MySqlConnection(DaoConexao.Conexao);
-                Conexao.Open();
-
-                MySqlCommand comando = new MySqlCommand();
-                comando.Connection = Conexao;
-                comando.Prepare();
-                comando.CommandText = "SELECT * FROM contato WHERE nome LIKE @nome OR email LIKE @nome";
-
-                comando.Parameters.AddWithValue("@nome", $"%{text}%");
+                else
+                {
+                    comando.CommandText = "SELECT * FROM contato";
+                }
                 MySqlDataReader reader = comando.ExecuteReader();
 
                 List<Cliente> clientes = new List<Cliente>();
@@ -117,7 +124,36 @@ namespace IniciandoComMysql.Dao
             {
                 Conexao.Close();
             }
+        }
 
+        public void Atualizar(Cliente cliente, int id)
+        {
+            try
+            {
+                Conexao = new MySqlConnection(DaoConexao.Conexao);
+                Conexao.Open();
+
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = Conexao;
+                comando.Prepare();
+
+                comando.CommandText = "UPDATE contato SET nome=@nome, email=@email, telefone=@telefone, endereco=@endereco WHERE id=@id";
+                comando.Parameters.AddWithValue("@id", cliente.Id);
+                comando.Parameters.AddWithValue("@nome", cliente.Nome);
+                comando.Parameters.AddWithValue("@email", cliente.Email);
+                comando.Parameters.AddWithValue("@telefone", cliente.Telefone);
+                comando.Parameters.AddWithValue("@endereco", cliente.Endereco);
+
+                comando.ExecuteReader();
+
+            } catch (MySqlException erro)
+            {
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
         }
     }
 
